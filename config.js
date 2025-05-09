b083f7818c55cfd83c0f76e8ffa1b75cbbde1986
Herontp <herontp@dukes.jmu.edu>
2025-05-09 15:10:31 -0400

    Update config.js

--- config.js
+++ config.js
@@ -24,7 +24,8 @@ var config = {
             id: 'opening',
             alignment: 'centered',
             hidden: false,
-            title: 'Thesis',
+            title: 'Thesis'
+            image: './assets/coltons_map_southern_states_INTRO',
             description: 'Before the works of William Faulkner, Southern literature focused almost exclusively on wealthy white landowners who traveled freely and made their own choices, whether economic or personal. This came at the expense of marginalized groups, who were often not fully developed characters but rather a part of the scenery. This tradition is broken, first by Faulkner, whose Bundren family demonstrates how economic desperation transcends racial lines, then by Huston, Walker, and Ward, who show a much more diverse—and much more mobile—South. The four novels thus represent a transition in focus from the planter elite to the everyday individual, who, despite harsh (and sometimes violent) constraints, ultimately have much more agency than is often portrayed.',
             location: {
                 center: [-88.19194, 33.14646],
